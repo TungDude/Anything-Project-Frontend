1eceae0c7d912cf7e83eadac5ef1f00dd2a1b659
KrittapopManupornprapa <krittapop.manupornprapa@safecoms.asia>
2025-01-11 00:08:58 +0700

    feat: layout footer

---
+++ anything-react/src/components/Layout/_components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const NavBar = () => {
+  return (
+    <nav>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/not-found">Not Found</Link></li>
+        <li><Link to="/random">Random</Link></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
