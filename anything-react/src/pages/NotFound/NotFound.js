33a5fa5900722e6d28ae43904f5d5ced7921e1b9
KrittapopManupornprapa <krittapop.manupornprapa@safecoms.asia>
2025-01-10 23:53:55 +0700

    feat: not found page

---
+++ anything-react/src/pages/NotFound/NotFound.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NotFound = () => {
+    return (
+        <>
+            <div>
+                <h1 className="text-xl text-cyan-300">Page Not Found</h1>
+            </div>
+        </>
+    );
+}
+
+export default NotFound;
