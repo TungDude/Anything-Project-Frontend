3b682b31390aac44a66b04c2a1337621cd861880
KrittapopManupornprapa <krittapop.manupornprapa@safecoms.asia>
2025-01-11 14:44:07 +0700

    fix: incorrect code placement

--- anything-react/src/components/Layout/_components/Footer/Footer.js
+++ anything-react/src/components/Layout/_components/Footer/Footer.js
@@ -1,16 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
-const NavBar = () => {
+const Footer = () => {
   return (
-    <nav>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/not-found">Not Found</Link></li>
-        <li><Link to="/random">Random</Link></li>
-      </ul>
-    </nav>
+    <footer>
+      <p>© 2025 TungDude</p>
+    </footer>
   );
 };
 
-export default NavBar;
+export default Footer;
