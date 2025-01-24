69a8d23b5a26a458da460fc57496b11df417f994
KrittapopManupornprapa <krittapop.kms@gmail.com>
2025-01-24 22:28:32 +0700

    fix: remove linkedin badge

--- anything-react/src/pages/AboutMe/AboutMe.js
+++ anything-react/src/pages/AboutMe/AboutMe.js
@@ -1,11 +1,19 @@
 import React from "react";
-import LinkedInBadge from "./_components/LinkedInBadge/LinkedInBadge";
+import { Link } from "react-router-dom";
+// import LinkedInBadge from "./_components/LinkedInBadge/LinkedInBadge";
 
 const AboutMe = () => {
     return (
         <>
             <div>
-                <LinkedInBadge />
+                {/* <LinkedInBadge /> */}
+                <Link
+                    to="https://www.linkedin.com/in/krittapop-manupornprapa/"
+                    target="_blank"
+                    rel="noopenner noreferrer"
+                >
+                    My LinkedIn
+                </Link>
             </div>
         </>
     );
