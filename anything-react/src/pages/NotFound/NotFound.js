57d90ce173067edcb6a239bb2d416d5e21e5e60d
KrittapopManupornprapa <krittapop.kms@gmail.com>
2025-01-24 22:28:24 +0700

    fix: text color

--- anything-react/src/pages/NotFound/NotFound.js
+++ anything-react/src/pages/NotFound/NotFound.js
@@ -4,7 +4,7 @@ const NotFound = () => {
     return (
         <>
             <div>
-                <h1 className="text-xl text-cyan-300">Page Not Found</h1>
+                <h1 className="text-xl text-cyan">Page Not Found</h1>
             </div>
         </>
     );
