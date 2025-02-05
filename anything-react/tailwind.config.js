2d397398fc125fb06bb9bf387c9be168c7dec334
KrittapopManupornprapa <krittapop.kms@gmail.com>
2025-02-05 16:20:31 +0700

    feat: add colors to tailwind config

--- anything-react/tailwind.config.js
+++ anything-react/tailwind.config.js
@@ -8,11 +8,13 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'green': '#50e050', 
-        'cyan': '#46cccc', 
-        'blue': '#3c8cc8', 
-        'lightgray': '#949494', 
-        'black': '#404040', 
+        'green': '#50e050',
+        'cyan': '#46cccc',
+        'blue': '#3c8cc8',
+        'lightgray': '#949494',
+        'black': '#404040',
+        'white': '#f7f9f9',
+        'red': '#fe3232',
       }
     },
   },
