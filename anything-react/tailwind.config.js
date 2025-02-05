1f9fc0f8d55a1d05813c99dd3ef0059389a053c6
KrittapopManupornprapa <krittapop.kms@gmail.com>
2025-02-05 17:10:29 +0700

    feat: animation typing

--- anything-react/tailwind.config.js
+++ anything-react/tailwind.config.js
@@ -15,7 +15,30 @@ module.exports = {
         'black': '#404040',
         'white': '#f7f9f9',
         'red': '#fe3232',
-      }
+      },
+      keyframes: {
+        typing: {
+          "0%": {
+            width: "0%",
+            visibility: "hidden"
+          },
+          "100%": {
+            width: "100%"
+          }
+        },
+        blink: {
+          "50%": {
+            borderColor: "transparent"
+          },
+          "100%": {
+            borderColor: "white",
+          }
+        }
+      },
+      animation: {
+          // typing: "typing 2s steps(20) infinite alternate, blink 1s infinite"
+          typing: "typing 6s steps(37), blink 1.4s infinite"
+        }
     },
   },
   variants: {
