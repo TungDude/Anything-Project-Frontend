f3c2bc53bff41950516a990441e4a7e848c40e16
KrittapopManupornprapa <krittapop.kms@gmail.com>
2025-02-05 16:22:45 +0700

    feat: input validation and password hiding

--- anything-react/src/pages/Register/Register.js
+++ anything-react/src/pages/Register/Register.js
@@ -1,13 +1,34 @@
 import React from "react";
 import RequestController from "../../controller/RequestController";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import Button from "../../components/Button/Button";
+import TextInput from "../../components/Input/TextInput/TextInput";
 
 const Register = () => {
     const username = useRef(null);
     const password = useRef(null);
+    const [isUsernameError, setIsUsernameError] = useState(false);
+    const [isPasswordError, setIsPasswordError] = useState(false);
+
+    const validateInput = () => {
+        const errors = {
+            username: !username.current.value,
+            password: !password.current.value,
+        };
+
+        setIsUsernameError(errors.username);
+        setIsPasswordError(errors.password);
+
+        return errors.username || errors.password;
+    };
 
     const handleClickCreateUser = () => {
+        const error = validateInput();
+
+        if (error) {
+            return;
+        }
+
         RequestController.CreateUser({
             username: username.current.value,
             password: password.current.value,
@@ -19,27 +40,52 @@ const Register = () => {
 
     return (
         <>
-            <div>
-                <h1 className="text-3xl text-blue">Create User</h1>
-                <div>
-                    <span className="mr-4">Username</span>
-                    <input
-                        type="text"
-                        ref={username}
-                    />
-                </div>
-                <div>
-                    <span className="mr-4">Password</span>
-                    <input
-                        type="text"
-                        ref={password}
-                    />
-                </div>
-                <Button
-                    label={"Create User"}
-                    onClick={handleClickCreateUser}
+            <h1
+                className="text-4xl text-blue font-bold"
+            >
+                Create User
+            </h1>
+            <div
+                className="flex flex-col"
+            >
+                <TextInput
+                    label={"Username"}
+                    ref={username}
                 />
+                {isUsernameError && (
+                    <>
+                        <span
+                            className="text-xs text-red"
+                        >
+                            Please enter a username
+                        </span>
+                    </>
+                )}
             </div>
+            <div
+                className="flex flex-col"
+            >
+                <TextInput
+                    label={"Password"}
+                    type={"password"}
+                    ref={password}
+                />
+                {isPasswordError && (
+                    <>
+                        <span
+                            className="text-xs text-red"
+                        >
+                            Please enter a password
+                        </span>
+                    </>
+                )}
+            </div>
+
+            <Button
+                label={"Register"}
+                onClick={handleClickCreateUser}
+                className={"my-2 w-full"}
+            />
         </>
     );
 }
