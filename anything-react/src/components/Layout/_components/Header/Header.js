9050d3aaa2c45d68c20d7c8171984c83d9f39bfb
KrittapopManupornprapa <krittapop.kms@gmail.com>
2025-02-05 16:28:02 +0700

    feat: login route

--- anything-react/src/components/Layout/_components/Header/Header.js
+++ anything-react/src/components/Layout/_components/Header/Header.js
@@ -21,7 +21,7 @@ const Header = () => {
                         src="/images/main_logo.png"
                         alt="Logo"
                     />
-                    <span className="font-semibold text-xl tracking-[4px] hover:text-gray-400">Anything Project</span>
+                    <span className="font-semibold text-xl tracking-[2px] hover:text-gray-400">Anything Project</span>
                     <img
                         className="h-16 w-auto" // Adjust size of the image as needed
                         src="/images/main_logo.png"
@@ -32,6 +32,11 @@ const Header = () => {
                 {/* Navigation aligned to the right */}
                 <nav>
                     <ul className="flex space-x-4">
+                        <li>
+                            <Link to="/login" className="text-white hover:text-gray-400 hover:underline">
+                                Login
+                            </Link>
+                        </li>
                         <li>
                             <Link to="/register" className="text-white hover:text-gray-400 hover:underline">
                                 Register
